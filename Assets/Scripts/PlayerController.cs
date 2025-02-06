187358c198866bc02a2bc98475322b8d4087622c
Duy Quang <quangduytc1709@gmail.com>
2025-02-06 20:32:51 +0700

    Add rule jump for player

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,6 +2,18 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public float jumpForce;
+    private Rigidbody mRigidbody;
+    private bool mCanJump = false;
+
+
+    private void Awake()
+    {
+        mRigidbody = GetComponent<Rigidbody>();
+
+    }
+
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,6 +23,22 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0) && mCanJump) { 
+            mRigidbody.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Ground")
+        {
+            mCanJump = true;
+        }
+    }
+
+    private void OnCollisionExit(Collision collision) {
+        if (collision.gameObject.tag == "Ground") {
+            mCanJump = false;
+        }
     }
 }
